4e77c95ffba9fc5afa1a13640af09a097baedb13
bvu <bryantvu91@gmail.com>
2020-05-25 02:05:16 -0700

    more code

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import numpy as np
 from riotwatcher import LolWatcher, ApiError
 
 # golbal variables
-api_key = 'RGAPI-d9f5ad6e-7546-490c-86e0-4e52dcd3dd8e'
+api_key = 'RGAPI-a1453a89-b310-46ef-9f36-1b7d3086aad4'
 watcher = LolWatcher(api_key)
 my_region = 'na1'
 
@@ -16,26 +16,9 @@ my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
 last_match = my_matches['matches'][0]
 match_detail = watcher.match.by_id(my_region, last_match['gameId'])
 
+last_match
 participants = []
-for row in match_detail['participants']:
-    participants_row = {}
-    participants_row['champion'] = row['championId']
-    participants_row['spell1'] = row['spell1Id']
-    participants_row['spell2'] = row['spell2Id']
-    participants_row['win'] = row['stats']['win']
-    participants_row['kills'] = row['stats']['kills']
-    participants_row['deaths'] = row['stats']['deaths']
-    participants_row['assists'] = row['stats']['assists']
-    participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
-    participants_row['goldEarned'] = row['stats']['goldEarned']
-    participants_row['champLevel'] = row['stats']['champLevel']
-    participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
-    participants_row['item0'] = row['stats']['item0']
-    participants_row['item1'] = row['stats']['item1']
-    participants.append(participants_row)
-df = pd.DataFrame(participants)
 
-df
 # check league's latest version
 latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
 # Lets get some champions static information
@@ -53,5 +36,3 @@ for row in participants:
 # print dataframe
 df = pd.DataFrame(participants)
 df
-
-test
