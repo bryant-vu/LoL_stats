83c83e3e6abf24f1398a5948620420bf520eeddd
bvu <bryantvu91@gmail.com>
2020-05-25 17:46:07 -0700

    work

--- code.py
+++ code.py
@@ -6,33 +6,18 @@ from riotwatcher import LolWatcher, ApiError
 api_key = 'RGAPI-a1453a89-b310-46ef-9f36-1b7d3086aad4'
 watcher = LolWatcher(api_key)
 my_region = 'na1'
+summoner_name = 'NotSoSrs'
 
-me = watcher.summoner.by_name(my_region, 'NotSoSrs')
-print(me)
+me = watcher.summoner.by_name(my_region, summoner_name)
 
-my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
+matches_df = pd.DataFrame()
+for x in range(0,99,100):
+    matches = watcher.match.matchlist_by_account(my_region, me['accountId'], begin_time=0, begin_index=x, end_index=x+99,season=[13])
+    for match_attr in matches['matches']: #this is a list
+        match_attr_list = match_attr.values()
+        matches_df = matches_df.append(pd.Series(match_attr_list),ignore_index=True)
+matches_df.columns = match_attr.keys()
 
-# fetch last match detail
-last_match = my_matches['matches'][0]
-match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+print(matches_df.columns)
 
-last_match
-participants = []
-
-# check league's latest version
-latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-# Lets get some champions static information
-static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
-
-# champ static list data to dict for looking up
-champ_dict = {}
-for key in static_champ_list['data']:
-    row = static_champ_list['data'][key]
-    champ_dict[row['key']] = row['id']
-for row in participants:
-    print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
-    row['championName'] = champ_dict[str(row['champion'])]
-
-# print dataframe
-df = pd.DataFrame(participants)
-df
+#champions = watcher.data_dragon.champions('10.10.3216176')
